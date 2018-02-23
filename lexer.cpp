b0cec9a3f39b1fd8ae2edf576548e773d3265ac4
johnsonlien <johnsonlien95@gmail.com>
2018-02-22 17:03:25 -0800

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -7,6 +7,23 @@ using namespace std;
 
 int main(int argc, char** argv) {
   
+  char filename[256];
+  
+  ifstream fs("test1.txt");   //Open file
+  
+  //Fheck that the file is open
+  if(!fs.is_open()) {
+    cout << "Could not find file. Aborting." << endl; 
+    return -1;
+  }
+  
+  while(!fs.eof()) {
+    string temp;
+    fs >> temp;
+    if(isValid(temp)) {
+      
+    }
+  }
   
   return 0;
 }
